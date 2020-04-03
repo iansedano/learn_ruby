4239d6c15009883f0e0c89613a369ebbeb280a5b
iansedano <iansedano@gmail.com>
2020-04-03 16:39:58 +0200

    finished excercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title=(value)
+		@title = titleize(value)
+	end
+
+	
+
+	def titleize(string)
+
+	prepositions = ["in", "at", "to", "from", "by", "over", "of"]
+	conjunctions = ["and"]
+	articles = ["the", "a", "an"]
+
+	string = string.capitalize
+	array = string.split
+	array = array.collect{|word|
+		if prepositions.include?(word.downcase) || conjunctions.include?(word.downcase) || articles.include?(word.downcase)
+			word
+		else
+			word.capitalize
+		end}
+	array.join(" ")
+	end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,32 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :time_string
+
+  def initialize()
+  	@seconds = 0
+  end
+
+  def seconds=(value)
+  @seconds = value
+  end
+
+  def time_string()
+  	if seconds == 0
+  		@time_string = "00:00:00"
+  	else
+	  	seconds_string = make_digit_string(seconds % 60)
+	  	minutes_string = make_digit_string((seconds / 60) % 60)
+	  	hours_string = make_digit_string((seconds / 60) / 60)
+	  	@time_string = hours_string + ":" + minutes_string + ":" + seconds_string
+  	end
+  end
+
+  def make_digit_string(int)
+  	int_string = int.to_s
+  	if int < 10
+		return "0" + int_string
+	else
+		int_string
+	end
+  end
+
 end
